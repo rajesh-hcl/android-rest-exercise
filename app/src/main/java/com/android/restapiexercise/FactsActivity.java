e9ecde1757ecacfdaf7fe29a42ac4ddc46a376ec
Rajesh <urajesh.android@gmail.com>
2018-02-16 15:09:39 +0530

    First commit - Project created

---
+++ app/src/main/java/com/android/restapiexercise/FactsActivity.java
@@ -0,0 +1,13 @@
+package com.android.restapiexercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facts);
+    }
+}
